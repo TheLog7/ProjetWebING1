b447517060c2d76d24b28a3a8ac4746c82b7feaf
BA Louqman <ba.louqman@gmail.com>
2025-03-24 12:32:03 +0100

    Suppression de redondances

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -25,22 +25,4 @@ final class HomePageController extends AbstractController{
         ]);
     }
 
-    //Route pour la page de connexion
-    #[Route('/page_connexion', name: 'app_login')]
-    public function page_connexion(): Response
-    {
-        return $this->render('page_connexion/index.html.twig', [
-            'controller_name' => 'HomePageController',
-        ]);
-    }
-
-    //Route pour la page d'inscription
-    #[Route('/page_inscription', name: 'app_signup')]
-    public function page_inscription(): Response
-    {
-        return $this->render('page_inscription/index.html.twig', [
-            'controller_name' => 'HomePageController',
-        ]);
-    }
-
 }
