0513f287c60367b486d2e0aec6004b04d97be50a
Florianne <186144350+Floriannee@users.noreply.github.com>
2025-04-01 10:25:50 +0200

    Affichage de la page objets connectés

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -9,8 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+
 
 #[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
